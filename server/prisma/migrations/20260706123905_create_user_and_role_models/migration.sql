cc9af56bbc25054d52792fac3c5a3f7cfa63c849
Hiroyokii <mr.snapy8@gmail.com>
2026-07-06 20:44:27 +0800

    feat(database): add user and role schema with initial migration

---
+++ server/prisma/migrations/20260706123905_create_user_and_role_models/migration.sql
@@ -0,0 +1,37 @@
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "middleName" TEXT,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "block" TEXT NOT NULL,
+    "lot" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "landmark" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "roleId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
