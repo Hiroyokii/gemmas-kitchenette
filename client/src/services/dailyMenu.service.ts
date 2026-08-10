91739a86ce055bbd4471d553d6ef5bd6ce245e34
Hiroyokii <mr.snapy8@gmail.com>
2026-08-10 20:40:14 +0800

    feat: add daily menu service

--- client/src/services/dailyMenu.service.ts
+++ client/src/services/dailyMenu.service.ts
@@ -1,9 +1,29 @@
 import api from "../api/axios";
+import type { DailyMenu } from "../types/DailyMenu";
 
-export async function getTodayMenu() {
+export async function getTodayMenu(): Promise<DailyMenu[]> {
     const response = await api.get(
-        "./daily-menu/today"
+        "/daily-menu/today"
     );
+
+    return response.data;
+}
+
+export async function getTodayMenuForAdmin(): Promise<DailyMenu[]> {
+    const response = await api.get("/daily-menu");
+
+    return response.data;
+}
+
+export interface PrepareFoodInput {
+    foodId: number;
+    preparedServings: number;
+}
+
+export async function prepareDailyFood(
+    data: PrepareFoodInput
+): Promise<DailyMenu> {
+    const response = await api.post("/daily-menu", data);
 
     return response.data;
 }
