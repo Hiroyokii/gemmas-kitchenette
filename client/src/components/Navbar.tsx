dbbe454f69b725c65a65ed66b99a56387bcdd2df
Hiroyokii <mr.snapy8@gmail.com>
2026-08-22 17:52:26 +0800

    refractor: remove components

--- client/src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-    return (
-        <nav className="border-b p-4">
-            <h1 className="text-xl font-bold">
-                Gemma's Kitchenette
-            </h1>
-        </nav>
-    );
-}
