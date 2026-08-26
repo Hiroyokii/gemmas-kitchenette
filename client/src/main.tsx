ba753b8f613e5acc1bea825e9c70586c86df5c5a
Hiroyokii <mr.snapy8@gmail.com>
2026-08-26 22:37:38 +0800

    chore: install and initialize tanstack for client side

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from "./contexts/AuthProvider.tsx";
 
 import "./style.css"
 
 import App from './App.tsx'
-import { CartProvider } from "./provider/CartProvider.tsx";
+import { CartProvider } from "./providers/CartProvider.tsx";
+import { AuthProvider } from "./providers/AuthProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <AuthProvider>
-    <CartProvider>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </CartProvider>
-  </AuthProvider>,
-)
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>,
+);
